a9f0f64172dd29f339baae3b7c8c033ddec29b9c
JolyHuang <JolyEmail@Gmail.COM>
2018-07-14 03:10:19 +0800

    init

--- com.sharingif.bips.parent/com.sharingif.bips.bip0039/src/test/java/com/sharingif/bips/bip0032/MnemonicTest.java
+++ com.sharingif.bips.parent/com.sharingif.bips.bip0039/src/test/java/com/sharingif/bips/bip0032/MnemonicTest.java
@@ -1,7 +1,5 @@
 package com.sharingif.bips.bip0032;
 
-import com.sharingif.bips.bip0032.wordlist.English;
-import com.sharingif.cube.security.binary.HexCoder;
 import org.junit.Test;
 
 import java.util.Locale;
@@ -19,14 +17,12 @@ public class MnemonicTest {
     @Test
     public void mnemonic() {
 
-
-        Entropy entropy = new Entropy(new HexCoder().decode("3ee74309d55715669d2649bbf63b9ab9"));
-        System.out.println(new HexCoder().encode(entropy.getEntropy()));
-
-        Mnemonic mnemonic = new Mnemonic(Locale.US, entropy);
+        Mnemonic mnemonic = new Mnemonic(Locale.US, 12);
 
         System.out.println(mnemonic.getMnemonic());
 
+
     }
+
 
 }
